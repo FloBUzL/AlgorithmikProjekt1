5575c9da2b1fb75674543460a32ff8faf4c89567
Florian Berberich <florian.berberich@student.uni-luebeck.de>
2018-11-13 19:57:31 +0100

    feat(intolp): updated the output so it fits the lp_solve syntax

--- InToLP.java
+++ InToLP.java
@@ -21,30 +21,51 @@ public class InToLP {
 	}
 	
 	public void printOut() {
+		StringBuilder max = new StringBuilder("max: ");
+		Wrapper<Boolean> following = new Wrapper<>(false);
 		this.collissions.getAllVariables().forEach((variable) -> {
-			System.out.println(variable + " <= 1");
+			if(following.getValue()) {
+				max.append(" + ");
+			}
+			max.append(variable);
+			following.setValue(true);
 		});
+		max.append(";");
+		System.out.println(max);
 		
-		System.out.println();
+		this.collissions.getAllVariables().forEach((variable) -> {
+			System.out.println(variable + " <= 1;");
+			System.out.println(variable + " >= 0;");
+		});
 		
 		this.collissions.getAllTitleCollissions().keySet().forEach((key) -> {
 			StringBuilder out = new StringBuilder(key);
 			this.collissions.getAllTitleCollissions().get(key).forEach((value) -> {
 				out.append(" + " + value);
 			});
-			out.append(" <= 1");
+			out.append(" <= 1;");
 			System.out.println(out.toString());
 		});
-		
-		System.out.println();
 		
 		this.collissions.getAllTimeCollissions().keySet().forEach((key) -> {
 			StringBuilder out = new StringBuilder(key);
 			this.collissions.getAllTimeCollissions().get(key).forEach((value) -> {
 				out.append(" + " + value);
 			});
-			out.append(" <= 1");
+			out.append(" <= 1;");
 			System.out.println(out.toString());
 		});
+		
+		StringBuilder type = new StringBuilder("int ");
+		following.setValue(false);
+		this.collissions.getAllVariables().forEach((variable) -> {
+			if(following.getValue()) {
+				type.append(", ");
+			}
+			type.append(variable);
+			following.setValue(true);
+		});
+		type.append(";");
+		System.out.println(type);
 	}
 }
