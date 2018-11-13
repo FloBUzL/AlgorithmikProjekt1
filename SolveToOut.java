761fd44817cb6d6802cf7caa0fb189aa45d47a38
Florian Berberich <florian.berberich@student.uni-luebeck.de>
2018-11-13 20:47:54 +0100

    feat(solvetoout): completed the task

--- SolveToOut.java
+++ SolveToOut.java
@@ -17,7 +17,6 @@ public class SolveToOut {
 		
 		Files.lines(Paths.get("./mapping.txt")).forEach((line) -> {
 			String[] splitted = line.split("\t");
-			System.out.println(splitted[1]);
 			this.collissions.addEntry(splitted[0], MovieData.createFromInputLine(splitted[1]));
 		});
 		
@@ -27,10 +26,15 @@ public class SolveToOut {
 	public void printOut() {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		in.lines().forEachOrdered((line) -> {
+			if(line.length() == 0 || line.charAt(0) != 'x') {
+				return;
+			}
 			String key = line.substring(0, 2);
-			String value = line.substring(-1);
-			System.out.println(key);
-			System.out.println(value);
+			int value = Integer.parseInt(line.substring(line.length() - 1));
+			
+			if(value == 1) {
+				System.out.println(this.collissions.getMapping().get(key).toString());
+			}
 		});
 	}
 }
