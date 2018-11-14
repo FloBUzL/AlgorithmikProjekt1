29f400762b82d9fa650d28775bb5330f8a79c64b
Florian Berberich <florian.berberich@student.uni-luebeck.de>
2018-11-14 12:37:07 +0100

    feat(nfl): started to implement the solver class

---
+++ NFL.java
@@ -0,0 +1,20 @@
+
+public class NFL {
+	public static void main(String[] args) {
+		new NFL().readIn().solve().printOut();
+	}
+	
+	public NFL readIn() {
+		
+		
+		return this;
+	}
+	
+	public NFL solve() {
+		return this;
+	}
+	
+	public void printOut() {
+		
+	}
+}
