3dc3f24218c7f182b766171f96af5b8377b82bcf
Florian Berberich <florian.berberich@student.uni-luebeck.de>
2018-11-15 20:14:37 +0100

    feat(nfl): did some work on the solver
    todo: solve the instances and compare the results

--- NFL.java
+++ NFL.java
@@ -1,13 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NFL {
 	private Collission collissions;
 	private String[] orderedByEndTime;
+	private HashMap<String, Integer> variableIndex;
 	private MovieData[] orderedByEndTimeData;
-	private int[] biggestFittingIndex;
-	private int[][] instances;
+	private int[] generalBiggestFittingIndex;
+	private SchedulingInstance[] instances;
+	private Set<String> excludes;
 	
 	public static void main(String[] args) throws IOException {
 		new NFL().readIn().prepare().solve().printOut();
@@ -25,6 +31,8 @@ public class NFL {
 	}
 	
 	public NFL prepare() {
+		this.variableIndex = new HashMap<>();
+		this.excludes = new HashSet<>();
 		this.orderedByEndTime = this.collissions.getAllVariables().sorted((str1,str2) -> {
 			TimeStamp left = this.collissions.getMapping().get(str1).getTime().getEndTime();
 			TimeStamp right = this.collissions.getMapping().get(str2).getTime().getEndTime();
@@ -36,10 +44,11 @@ public class NFL {
 			return left.compareTo(right);
 		}).toArray(size -> new String[size]);
 		
-		this.biggestFittingIndex = new int[this.orderedByEndTime.length];
+		this.generalBiggestFittingIndex = new int[this.orderedByEndTime.length];
 		this.orderedByEndTimeData = new MovieData[this.orderedByEndTime.length];
 		
 		for(int i = 0;i < this.orderedByEndTime.length;i++) {
+			this.variableIndex.put(this.orderedByEndTime[i], i);
 			this.orderedByEndTimeData[i] = this.collissions.getMapping().get(this.orderedByEndTime[i]);
 			int currVal = -1;
 			for(int j = 0;j < i;j++) {
@@ -47,8 +56,28 @@ public class NFL {
 					currVal = j;
 				}
 			}
-			this.biggestFittingIndex[i] = currVal;
+			this.generalBiggestFittingIndex[i] = currVal;
 		}
+		
+		InstanceIterator iterator = new InstanceIterator();
+		this.collissions.getAllTitleCollissions().forEach((str, list) -> {
+			list.add(str);
+			this.excludes.addAll(list);
+			iterator.addChild(list);
+		});
+		iterator.execute();
+		
+		iterator.getSchedulingInstances().forEach((instance) -> {
+			this.variableIndex.forEach((str, i) -> {
+				if(this.excludes.contains(str)) {
+					return;
+				}
+				instance.addEntry(str);
+			});
+			instance.setUpIndexList(this.orderedByEndTime, this.orderedByEndTimeData, this.variableIndex);
+			instance.setUpLastFittingIndex();
+			instance.print();
+		});
 		
 		return this;
 	}
